9d1a910d9e14fe2905d7866375bb382f8dbbd5f7
TheNormalStudent <slavik.zhukoff@gmail.com>
2023-07-03 17:16:03 +0300

    Create Base Rate model

--- app/currency/models.py
+++ app/currency/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+class Rate(models.Model):
+    sale = models.DecimalField(max_digits=5, decimal_places=2)
+    buy = models.DecimalField(max_digits=5, decimal_places=2) #123.45
+    created = models.DateTimeField(auto_now_add = True)
+    source = models.CharField(max_length=32) #examples: privatbank, monobank
+    type = models.CharField(max_length=3) #examples: USD, EUR 
