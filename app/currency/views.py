2496fdeedbfcb5cf1706fa3e4d66448cfce8eaf6
TheNormalStudent <slavik.zhukoff@gmail.com>
2023-07-05 17:28:36 +0300

    Added view func to show list of Rates (model)

--- app/currency/views.py
+++ app/currency/views.py
@@ -1,7 +1,8 @@
+from currency.models import Rate
 from currency.utils import generate_password as gen_pass
 
 from django.http import HttpResponse as HR
-from django.shortcuts import render
+from django.shortcuts import render # noqa
 
 # Create your views here.
 
@@ -14,3 +15,13 @@ def generate_password(request):
     password_len = int(request.GET.get('password-len'))
     password = gen_pass(password_len)
     return HR(password)
+
+
+def rate_list(request):
+    rates = Rate.objects.all()
+
+    result = []
+    for rate in rates:
+        result.append(f'Rate: {rate.id} Sale: {rate.sale} Buy: {rate.buy} </br>')
+
+    return HR(str(result))
--- app/settings/urls.py
+++ app/settings/urls.py
@@ -1,10 +1,11 @@
+from currency.views import generate_password, hello_world, rate_list
+
 from django.contrib import admin
 from django.urls import path
-
-from currency.views import hello_world, generate_password
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('hello-world/', hello_world),
-    path('generate-password/', generate_password)
+    path('generate-password/', generate_password),
+    path('rate/list', rate_list)
 ]
